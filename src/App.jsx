1915fc015ba9b0ef492f2f5205f77907d48bacdf
BlackTurbo <sohandsome060@gmail.com>
2024-09-17 14:28:21 +0800

    final change

--- src/App.jsx
+++ src/App.jsx
@@ -3,48 +3,77 @@ import './App.css'
 
 function Caculate(){
   const [display, setDisplay] = useState(0)
-  const [output, setOutput] = useState(0);
   const [operator, setOperator]=  useState('');
-  const [number1, setNumber1] = useState(null);
+  const [number1, setNumber1] = useState(0);
   const [number2, setNumber2] = useState(null);
-  const [decimal, setDecimal] = useState(null);
   const [haveDecimal, setHaveDecimal] = useState(false);
+  const [decimalFactor, setDecimalFactor] = useState(0.1); 
 
   const handleButtonClick = (value) => {
     if(['+', '-', '*', '/'].includes(value) && number1 !== null){
       setOperator(value);
+      setHaveDecimal(false);
+      setDecimalFactor(0.1);
     }else if(value === '='){
+      let result = 0;
       switch (operator) {
         case '+':
-          setOutput(number1 + number2)
+          result = number1 + number2;
           break;
         case '-':
-          setOutput(number1 - number2)
+          result = number1 - number2;
           break;
         case '*':
-          setOutput(number1 * number2)
+          result = number1 * number2;
           break;
         case '/':
-          setOutput(number1 / number2)
+          result = number2 !==0 ? number1 / number2 : 'Error';
           break;
         default:
           break;
       }
+      const roundedResult = parseFloat(result.toFixed(10));
+      setDisplay(roundedResult);
+      setNumber1(roundedResult);
+      setNumber2(null);
+      setHaveDecimal(false);
+      setDecimalFactor(0.1);
     }else if(value === 'AC'){
-      setNumber1(null);
+      setNumber1(0);
       setNumber2(null);
       setOperator('');
-      setOutput(0);
-    }else if(value === '.'){
+      setDisplay(0);
+      setHaveDecimal(false);
+      setDecimalFactor(0.1);
+    }else if(value === '.' && !haveDecimal){
       setHaveDecimal(true);
+      const newDisplay = display + '.';
+      setDisplay(newDisplay);
     }else{
       if(number1 !== null && operator !== ''){
-        setNumber2(10 * number2 + value);
-        setDisplay(number2);
+        if(haveDecimal){
+          const newNumber2 = parseFloat((number2 + parseFloat(value) * decimalFactor).toFixed(10));
+          setNumber2(newNumber2);
+          setDisplay(newNumber2.toString());
+          setDecimalFactor(decimalFactor/10);
+        }else{
+          const newNumber2 = number2 * 10 + parseFloat(value)
+          setNumber2(newNumber2);
+          setDisplay(newNumber2.toString());  
+        }
+      }else{
+        if(haveDecimal){
+          const newNumber1 = parseFloat(((number1 + parseFloat(value) * decimalFactor)).toFixed(10));
+          console.log(newNumber1);
+          setNumber1(newNumber1);
+          setDisplay(newNumber1.toString());
+          setDecimalFactor(decimalFactor/10);
+        }else{
+          const newNumber1 = number1 * 10 + parseFloat(value);
+          setNumber1(newNumber1);
+          setDisplay(newNumber1.toString());
+        }
       }
-      setNumber1(10 * number1 + parseInt(value));
-      console.log(number1);
-      setDisplay(parseInt(number1 + value));
     }
   }
   
@@ -54,7 +83,7 @@ function Caculate(){
       <div id='container'>
         <div id='left'>
           <button id='AC' className='double' onClick={() => handleButtonClick('AC')}>AC</button>
-          <button id='divide' onClick={() => handleButtonClick('/')}>/</button>
+          <button id='divide' onClick={() => handleButtonClick('/')} className= {divide? 'selected' : null}>/</button>
           <button id='7' onClick={() => handleButtonClick('7')}>7</button>
           <button id='8' onClick={() => handleButtonClick('8')}>8</button>
           <button id='9' onClick={() => handleButtonClick('9')}>9</button>
@@ -68,10 +97,10 @@ function Caculate(){
           <button id='decimal' onClick={() => handleButtonClick('.')}>.</button>
         </div>
         <div id='right'>
-          <button id='multiply' onClick={() => handleButtonClick('*')}>*</button>
-          <button id='subtract' onClick={() => handleButtonClick('-')}>-</button>
-          <button id='add' onClick={() => handleButtonClick('+')}>+</button>
-          <button id='equal' className='vertical-double' onClick={() => handleButtonClick('=')}>=</button>
+          <button id='multiply' onClick={() => handleButtonClick('*')} className='selected'>*</button>
+          <button id='subtract' onClick={() => handleButtonClick('-')} className='selected'>-</button>
+          <button id='add' onClick={() => handleButtonClick('+')} className='selected'>+</button>
+          <button id='equal' onClick={() => handleButtonClick('=')} className= 'vertical-double selected'>=</button>
         </div>
       </div>
     </>
